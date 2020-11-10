dfc3e2a1a41439ca9402d01cc18a3dbdad77e479
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-11-10 23:41:29 +0100

    Add print and testing dto functionality

--- src/main/java/org/example/CustomerClientApp.java
+++ src/main/java/org/example/CustomerClientApp.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class CustomerClientApp {
 
     public static void main(String[] args) {
-        List<CustomerDTO> customerDTOList = new ArrayList<>();
+        List<CustomerDTO> customers = new ArrayList<>();
         CustomerDTO customer1 = new CustomerDTO("1", "John", "Smith");
         CustomerDTO customer2 = new CustomerDTO("2", "Kevin", "Biden");
 
-        customerDTOList.add(customer1);
-        customerDTOList.add(customer2);
+        customers.add(customer1);
+        customers.add(customer2);
 
-        CustomerResource customerResource = new CustomerResource(customerDTOList);
+        CustomerResource customerResource = new CustomerResource(customers);
 
         System.out.println("All customers: ");
         List<CustomerDTO> allCustomers = customerResource.getAllCustomers();
@@ -22,6 +22,22 @@ public class CustomerClientApp {
         System.out.println("------------------------------------");
 
         System.out.println("Deleting customer with id {1}");
+        customerResource.delete(customer1.getId());
+        allCustomers = customerResource.getAllCustomers();
+        printCustomerDetails(allCustomers);
 
+
+        System.out.println("------------------------------------");
+        System.out.println("Adding customer 3");
+        CustomerDTO customer3 = new CustomerDTO("3", "Barack", "Obama");
+        customerResource.save(customer3);
+        allCustomers = customerResource.getAllCustomers();
+        printCustomerDetails(allCustomers);
+
+
+    }
+
+    private static void printCustomerDetails(List<CustomerDTO> allCustomers) {
+        allCustomers.forEach(customer -> System.out.println(customer.getFirstname()));
     }
 }
